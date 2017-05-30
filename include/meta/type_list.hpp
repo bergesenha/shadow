69967d0efaa86d644b6ab97c2e98b84f2918a6de
Harald August Bergesen <bergesenha@gmail.com>
2017-05-30 15:41:17 +0200

    Add integer_sequence_from_type_list metafunction. 	modified:   include/meta/type_list.hpp

--- include/meta/type_list.hpp
+++ include/meta/type_list.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <utility>
+
 
 namespace shadow
 {
@@ -9,5 +11,19 @@ template <class...>
 struct type_list
 {
 };
+
+
+template <class TypeList>
+struct integer_sequence_from_type_list;
+
+template <class... Types>
+struct integer_sequence_from_type_list<type_list<Types...>>
+{
+    typedef std::index_sequence_for<Types...> type;
+};
+
+template <class TypeList>
+using integer_sequence_from_type_list_t =
+    typename integer_sequence_from_type_list<TypeList>::type;
 }
 }
