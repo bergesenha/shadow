b715d7e7483220b8d629edf40a80ccb107286e39
Harald August Bergesen <bergesenha@gmail.com>
2017-05-30 14:31:33 +0200

    Add partial implementation of return_type_specializer.
    Intended to select on return type of a free function to provide
    separate implementations for calling the function.
    	modified:   include/reflection_binding.hpp

--- include/reflection_binding.hpp
+++ include/reflection_binding.hpp
@@ -7,6 +7,33 @@ namespace shadow
 typedef any (*free_function_binding_signature)(any*);
 
 
+////////////////////////////////////////////////////////////////////////////////
+// generic bind point for free functions
+// has the same signature as function pointer free_function_binding_signature
+// which can be stored in the free_function_info struct
+
+
+namespace free_function_detail
+{
+// necessary to handle void as return type differently when calling underlying
+// free function
+template <class ReturnType>
+struct return_type_specializer
+{
+};
+
+
+template <>
+struct return_type_specializer<void>
+{
+};
+}
+
+
+// the value of the function pointer is stored at runtime in the template
+// overload set
+// this enables any function to be wrapped into uniform function signature that
+// can be stored homogenously at runtime
 template <class FunctionPointerType, FunctionPointerType FunctionPointerValue>
 any
 generic_free_function_bind_point(any* argument_array)
