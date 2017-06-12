3f9161844d1942f32cd01f5a2fb09555df14e130
Harald August Bergesen <bergesenha@gmail.com>
2017-06-12 22:38:52 +0200

    Add unit test where braced-init-list initialization is chosen as bind point. 	modified:   tests/test_constructor_binding.cpp

--- tests/test_constructor_binding.cpp
+++ tests/test_constructor_binding.cpp
@@ -35,6 +35,7 @@ TEST_CASE("test constructor binding", "[generic_constructor_bind_point]")
         &shadow::constructor_detail::
             generic_constructor_bind_point<test_construct2, char, double>;
 
+
     SECTION("construct int with int")
     {
         shadow::any intarg = 11;
@@ -53,8 +54,8 @@ TEST_CASE("test constructor binding", "[generic_constructor_bind_point]")
         REQUIRE(result.get<int>() == 23);
     }
 
-    SECTION("construct test_construct1 with int and float, constructor should "
-            "be called")
+    SECTION("when constructing a test_construct1 with int, float args, the "
+            "constructor should be chosen")
     {
         shadow::any args[] = {10, 23.4f};
 
@@ -62,5 +63,16 @@ TEST_CASE("test constructor binding", "[generic_constructor_bind_point]")
 
         REQUIRE(result.get<test_construct1>().i == 20);
         REQUIRE(result.get<test_construct1>().f == 23.4f);
+    }
+
+    SECTION("when constructing a test_construct2, braced-init-list "
+            "construction should be chosen")
+    {
+        shadow::any args[] = {'a', 25.3};
+
+        auto result = test_construct2_constructor(args);
+
+        REQUIRE(result.get<test_construct2>().c == 'a');
+        REQUIRE(result.get<test_construct2>().d == 25.3);
     }
 }
