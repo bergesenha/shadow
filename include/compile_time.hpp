cd9bb66437aaf16911d447dfdead257e17e0d5e9
Harald August Bergesen <bergesenha@gmail.com>
2017-06-15 12:10:48 +0200

    Add instantiated_compile_time_infos. 	modified:   include/compile_time.hpp 	modified:   src/main.cpp

--- include/compile_time.hpp
+++ include/compile_time.hpp
@@ -2,6 +2,8 @@
 
 #include <type_list.hpp>
 #include <integer_sequence.hpp>
+#include <sfinae.hpp>
+
 
 namespace shadow
 {
@@ -12,10 +14,33 @@ struct extract_name
     static constexpr const char* value = CompileTimeTypeInfo::name;
 };
 
+
 template <class TypeListOfCompileTimeTypeInfo>
 using generate_vector_of_strings =
     metamusil::t_list::value_transform<TypeListOfCompileTimeTypeInfo,
                                        extract_name>;
+
+
+template <template <std::size_t> class CTI, class IntSeqRange>
+struct generate_valid_compile_time_type_infos
+{
+    template <typename IntSeqRange::value_type LineNum>
+    using is_defined_predicate =
+        metamusil::integral_specialization_defined<std::size_t, CTI, LineNum>;
+
+    typedef metamusil::int_seq::filter_t<IntSeqRange, is_defined_predicate>
+        filtered_line_range;
+
+    typedef metamusil::t_list::from_integer_template_instantiations_t<
+        std::size_t,
+        CTI,
+        filtered_line_range>
+        type;
+};
+
+template <template <std::size_t> class CTI, class IntSeqRange>
+using generate_valid_compile_time_type_infos_t =
+    typename generate_valid_compile_time_type_infos<CTI, IntSeqRange>::type;
 }
 
 #define REGISTER_TYPE_BEGIN()                                                  \
@@ -43,4 +68,9 @@ using generate_vector_of_strings =
         type_line_begin + 1,                                                   \
         type_line_end>                                                         \
         type_line_range;                                                       \
-    \
+                                                                               \
+    typedef shadow::generate_valid_compile_time_type_infos_t<                  \
+        compile_time_type_info,                                                \
+        type_line_range>                                                       \
+        instantiated_compile_time_infos;
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 
+#include <compile_time.hpp>
+
+namespace myspace
+{
+REGISTER_TYPE_BEGIN()
+REGISTER_TYPE(int)
+REGISTER_TYPE(float)
+
+REGISTER_TYPE(double)
+REGISTER_TYPE_END()
+}
+
+
+template <class T>
+void p(T)
+{
+    puts(__PRETTY_FUNCTION__);
+}
+
 
 int
 main()
 {
-    std::cout << "Hello world\n";
+    p(myspace::instantiated_compile_time_infos());
 }
