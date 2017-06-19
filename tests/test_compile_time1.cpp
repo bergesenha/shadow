7fc7e234437376d10344d87854a039ab1dca611d
Harald August Bergesen <bergesenha@gmail.com>
2017-06-19 13:27:37 +0200

    Add unit tests for the case of registering no types. 	modified:   tests/test_compile_time1.cpp

--- tests/test_compile_time1.cpp
+++ tests/test_compile_time1.cpp
@@ -4,16 +4,27 @@
 #include <string>
 
 
-namespace myspace
+namespace refl_space_1
 {
 REGISTER_TYPE_BEGIN()
-REGISTER_TYPE(int)
-REGISTER_TYPE(float)
-REGISTER_TYPE(double)
 REGISTER_TYPE_END()
 }
 
 
-TEST_CASE("", "")
+TEST_CASE("No types registered", "[REGISTER_TYPE]")
 {
+    constexpr auto range_len =
+        metamusil::int_seq::length_v<refl_space_1::type_line_range>;
+
+    constexpr auto compile_time_infos_len = metamusil::t_list::length_v<
+        refl_space_1::instantiated_compile_time_infos>;
+
+    constexpr auto type_len =
+        metamusil::t_list::length_v<refl_space_1::type_universe>;
+
+    REQUIRE(range_len == 0);
+    REQUIRE(compile_time_infos_len == 0);
+    REQUIRE(refl_space_1::type_name_array_holder::value == nullptr);
+    REQUIRE(refl_space_1::type_info_array_holder::value == nullptr);
+    REQUIRE(type_len == 0);
 }
