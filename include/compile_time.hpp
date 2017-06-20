1b10b5df609215a4df26c3eac103a26ab4e13c5d
Harald August Bergesen <bergesenha@gmail.com>
2017-06-20 13:40:13 +0200

    Add fields to compile_time_ff_info template. 	modified:   include/compile_time.hpp

--- include/compile_time.hpp
+++ include/compile_time.hpp
@@ -5,6 +5,7 @@
 #include <type_list.hpp>
 #include <integer_sequence.hpp>
 #include <sfinae.hpp>
+#include <function_deduction.hpp>
 
 #include "reflection_info.hpp"
 #include "director.hpp"
@@ -431,14 +432,37 @@ constexpr conversion_info
     struct compile_time_ff_info;
 
 
-#define REGISTER_FREE_FUNCTION(function_name, ...)                             \
-                                                                               \
+#define REGISTER_FREE_FUNCTION(function_name)                                  \
                                                                                \
     template <>                                                                \
     struct compile_time_ff_info<__LINE__>                                      \
     {                                                                          \
         static constexpr char name[] = #function_name;                         \
-    };
+                                                                               \
+        typedef decltype(&function_name) function_pointer_type;                \
+                                                                               \
+        static const std::size_t return_type_index =                           \
+            metamusil::t_list::index_of_type_v<                                \
+                type_universe,                                                 \
+                metamusil::deduce_return_type_t<function_pointer_type>>;       \
+                                                                               \
+        typedef metamusil::deduce_parameter_types_t<function_pointer_type>     \
+            parameter_list;                                                    \
+                                                                               \
+        typedef metamusil::t_list::order_t<parameter_list, type_universe>      \
+            parameter_index_sequence;                                          \
+                                                                               \
+        typedef metamusil::int_seq::integer_sequence_to_array<                 \
+            parameter_index_sequence>                                          \
+            parameter_type_indices_holder;                                     \
+                                                                               \
+        static constexpr shadow::free_function_binding_signature value =       \
+            &shadow::free_function_detail::generic_free_function_bind_point<   \
+                decltype(&function_name),                                      \
+                &function_name>;                                               \
+    };                                                                         \
+                                                                               \
+    constexpr char compile_time_ff_info<__LINE__>::name[];
 
 
 #define REGISTER_FREE_FUNCTION_END()                                           \
