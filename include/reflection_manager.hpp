64cf07410b9801d7a2e0435eb49ee74a14ddd238
Harald August Bergesen <bergesenha@gmail.com>
2017-06-23 10:59:38 +0200

    Add new class reflection_manager to replace director. 	new file:   include/reflection_manager.hpp

---
+++ include/reflection_manager.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <utility>
+
+#include "reflection_info.hpp"
+
+
+namespace shadow
+{
+class reflection_manager
+{
+public:
+private:
+    std::pair<const type_info*, const type_info*> type_info_range_;
+    std::pair<const constructor_info*, const constructor_info*>
+        constructor_info_range_;
+    std::pair<const conversion_info*, const conversion_info*>
+        conversion_info_range_;
+    std::pair<const string_serialization_info*,
+              const string_serialization_info*>
+        string_serialization_info_range_;
+    std::pair<const free_function_info*, const free_function_info*>
+        free_function_info_range_;
+    std::pair<const member_function_info*, const member_function_info*>
+        member_function_info_range_;
+    std::pair<const member_variable_info*, const member_variable_info*>
+        member_variable_info_range_;
+};
+}
