4a8a20512e398c2b089930c1eb16a29bd7ae6946
Harald August Bergesen <bergesenha@gmail.com>
2017-06-23 13:31:03 +0200

    Set up array of type_info for testing type_iterator_. 	modified:   tests/test_iterators.cpp

--- tests/test_iterators.cpp
+++ tests/test_iterators.cpp
@@ -1,9 +1,13 @@
 #include "catch.hpp"
 
 #include <reflection_manager.hpp>
+#include <iterator>
 
 
 TEST_CASE("test type_iterator_", "[type_iterator_]")
 {
-    // move constructible
+    constexpr shadow::type_info type_info_array[] = {
+        {"type1", 1}, {"type2", 2}, {"type3", 3}, {"type4", 4},
+    };
+
 }
