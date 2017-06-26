1b359e57e725a6de5aa08226a7206d0cfe2b46c2
Harald August Bergesen <bergesenha@gmail.com>
2017-06-26 11:51:57 +0200

    Move api_type_aggregatore class template and instantiations to file. 	new file:   include/api_info_types.hpp 	modified:   include/reflection_manager.hpp

---
+++ include/api_info_types.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <string>
+
+#include "reflection_info.hpp"
+
+namespace shadow
+{
+class reflection_manager;
+
+
+template <class InfoType, template <class> class... Policies>
+class api_type_aggregator
+    : public Policies<api_type_aggregator<InfoType, Policies...>>...
+{
+public:
+    api_type_aggregator() : info_(nullptr), manager_(nullptr)
+    {
+    }
+
+    api_type_aggregator(const InfoType* info, const reflection_manager* manager)
+        : info_(info), manager_(manager)
+    {
+    }
+
+    api_type_aggregator* operator->()
+    {
+        return this;
+    }
+
+    const api_type_aggregator* operator->() const
+    {
+        return this;
+    }
+
+public:
+    const InfoType* info_;
+    const reflection_manager* manager_;
+};
+
+
+template <class Derived>
+class get_name_policy
+{
+public:
+    std::string
+    name() const
+    {
+        return (static_cast<const Derived*>(this))->info_->name;
+    }
+};
+
+
+template <class Derived>
+class get_size_policy
+{
+public:
+    std::size_t
+    size() const
+    {
+        return static_cast<const Derived*>(this)->info_->size;
+    }
+};
+
+
+typedef api_type_aggregator<type_info, get_name_policy, get_size_policy> type_;
+
+template <class Derived>
+class get_type_policy
+{
+public:
+    type_
+    get_type() const
+    {
+        // retrieve type index
+        const auto type_index =
+            static_cast<const Derived*>(this)->info_->type_index;
+
+        return static_cast<const Derived*>(this)->manager_->type_by_index(
+            type_index);
+    }
+};
+
+typedef api_type_aggregator<constructor_info, get_type_policy> constructor_;
+};
--- include/reflection_manager.hpp
+++ include/reflection_manager.hpp
@@ -6,6 +6,7 @@
 
 #include "reflection_info.hpp"
 #include "info_iterator.hpp"
+#include "api_info_types.hpp"
 
 namespace shadow
 {
@@ -25,81 +26,6 @@ private:
     const reflection_manager* manager_;
     std::size_t type_index_;
 };
-
-
-template <class InfoType, template <class> class... Policies>
-class api_type_aggregator
-    : public Policies<api_type_aggregator<InfoType, Policies...>>...
-{
-public:
-    api_type_aggregator() : info_(nullptr), manager_(nullptr)
-    {
-    }
-
-    api_type_aggregator(const InfoType* info, const reflection_manager* manager)
-        : info_(info), manager_(manager)
-    {
-    }
-
-    api_type_aggregator* operator->()
-    {
-        return this;
-    }
-
-    const api_type_aggregator* operator->() const
-    {
-        return this;
-    }
-
-public:
-    const InfoType* info_;
-    const reflection_manager* manager_;
-};
-
-
-template <class Derived>
-class get_name_policy
-{
-public:
-    std::string
-    name() const
-    {
-        return (static_cast<const Derived*>(this))->info_->name;
-    }
-};
-
-
-template <class Derived>
-class get_size_policy
-{
-public:
-    std::size_t
-    size() const
-    {
-        return static_cast<const Derived*>(this)->info_->size;
-    }
-};
-
-
-typedef api_type_aggregator<type_info, get_name_policy, get_size_policy> type_;
-
-template <class Derived>
-class get_type_policy
-{
-public:
-    type_
-    get_type() const
-    {
-        // retrieve type index
-        const auto type_index =
-            static_cast<const Derived*>(this)->info_->type_index;
-
-        return static_cast<const Derived*>(this)->manager_->type_by_index(
-            type_index);
-    }
-};
-
-typedef api_type_aggregator<constructor_info, get_type_policy> constructor_;
 
 
 class reflection_manager
