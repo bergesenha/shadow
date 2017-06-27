533f117623da93b9533a014f241fc719c5b8f464
Harald August Bergesen <bergesenha@gmail.com>
2017-06-27 15:16:08 +0200

    Remove constructor_info_by_index_ member. 	modified:   include/reflection_manager.hpp

--- include/reflection_manager.hpp
+++ include/reflection_manager.hpp
@@ -155,7 +155,6 @@ private:
         member_variable_info_range_;
 
     // sorted information
-    std::vector<std::vector<constructor_info>> constructor_info_by_index_;
     std::vector<std::vector<conversion_info>> conversion_info_by_index_;
     std::vector<std::vector<member_function_info>>
         member_function_info_by_index_;
@@ -201,10 +200,6 @@ inline reflection_manager::reflection_manager(
           initialize_range(MemberFunctionInfoArrayHolder())),
       member_variable_info_range_(
           initialize_range(MemberVariableInfoArrayHolder())),
-      constructor_info_by_index_(
-          buckets_by_index(constructor_info_range_,
-                           TypeInfoArrayHolder(),
-                           [](const auto& info) { return info.type_index; })),
       conversion_info_by_index_(buckets_by_index(
           conversion_info_range_,
           TypeInfoArrayHolder(),
