889d91f390c022fdf9263ead4d102a8a06c08546
Harald August Bergesen <bergesenha@gmail.com>
2017-10-11 11:09:12 +0200

    Add more unit tests. 	modified:   tests/test_member_function_api.cpp

--- tests/test_member_function_api.cpp
+++ tests/test_member_function_api.cpp
@@ -79,4 +79,67 @@ TEST_CASE("set up shadow::variable containing a tmfapi object",
             tmfapi_space::static_value_cast<tmfapi1>(test_obj).public_value ==
             100);
     }
+
+    SECTION("find fun2 and call it")
+    {
+        auto found = std::find_if(
+            mem_fun_pair.first, mem_fun_pair.second, [](const auto& mf) {
+                return mf.name() == std::string("fun2");
+            });
+
+        REQUIRE(found != mem_fun_pair.second);
+
+        shadow::variable args[] = {tmfapi_space::static_create<int>(55)};
+
+        auto res = test_obj.call_member_function(
+            *found, std::begin(args), std::end(args));
+
+        REQUIRE(res.type().name() == std::string("void"));
+        REQUIRE(
+            tmfapi_space::static_value_cast<tmfapi1>(test_obj).public_value ==
+            55);
+    }
+
+    SECTION("find fun3 and call it")
+    {
+        auto found = std::find_if(
+            mem_fun_pair.first, mem_fun_pair.second, [](const auto& mf) {
+                return mf.name() == std::string("fun3");
+            });
+
+        REQUIRE(found != mem_fun_pair.second);
+
+        std::vector<shadow::variable> empty_args;
+
+        auto res = test_obj.call_member_function(
+            *found, empty_args.begin(), empty_args.end());
+
+        REQUIRE(res.type().name() == std::string("int"));
+        REQUIRE(tmfapi_space::static_value_cast<int>(res) == 23);
+        REQUIRE(
+            tmfapi_space::static_value_cast<tmfapi1>(test_obj).public_value ==
+            20);
+    }
+
+    SECTION("find fun4 and call it")
+    {
+        auto found = std::find_if(
+            mem_fun_pair.first, mem_fun_pair.second, [](const auto& mf) {
+                return mf.name() == std::string("fun4");
+            });
+
+        REQUIRE(found != mem_fun_pair.second);
+
+        std::vector<shadow::variable> args{
+            tmfapi_space::static_create<int>(30)};
+
+        auto res =
+            test_obj.call_member_function(*found, args.begin(), args.end());
+
+        REQUIRE(res.type().name() == std::string("int"));
+        REQUIRE(tmfapi_space::static_value_cast<int>(res) == 60);
+        REQUIRE(
+            tmfapi_space::static_value_cast<tmfapi1>(test_obj).public_value ==
+            20);
+    }
 }
