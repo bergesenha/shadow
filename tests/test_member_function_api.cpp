53d302be0b93d8e1cb326476c4d7f8166668679d
Harald August Bergesen <bergesenha@gmail.com>
2017-10-11 09:52:11 +0200

    Add test class and register info in reflection system for tests. 	modified:   tests/test_member_function_api.cpp

--- tests/test_member_function_api.cpp
+++ tests/test_member_function_api.cpp
@@ -1,4 +1,48 @@
 #include "catch.hpp"
 
+#include <shadow.hpp>
+
+class tmfapi1
+{
+public:
+    int public_value;
+
+    void
+    fun1()
+    {
+        public_value = 100;
+    }
+
+    void
+    fun2(int i)
+    {
+        public_value = i;
+    }
+
+    int
+    fun3() const
+    {
+        return 23;
+    }
+
+    int
+    fun4(int i) const
+    {
+        return 2 * i;
+    }
+};
 
 
+namespace tmpfapi_space
+{
+REGISTER_TYPE_BEGIN()
+REGISTER_TYPE(tmfapi1)
+REGISTER_TYPE_END()
+
+REGISTER_MEMBER_FUNCTION(tmfapi1, fun1)
+REGISTER_MEMBER_FUNCTION(tmfapi1, fun2)
+REGISTER_MEMBER_FUNCTION(tmfapi1, fun3)
+REGISTER_MEMBER_FUNCTION(tmfapi1, fun4)
+
+SHADOW_INIT()
+}
