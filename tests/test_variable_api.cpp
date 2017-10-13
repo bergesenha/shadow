7e2c4c9d1c12239ea645c8aaf214f2a3e03f0760
Harald August Bergesen <bergesenha@gmail.com>
2017-10-13 12:38:40 +0200

    Add unit tests for default constructing shadow::variable. 	modified:   tests/test_variable_api.cpp

--- tests/test_variable_api.cpp
+++ tests/test_variable_api.cpp
@@ -1,17 +1,52 @@
 #include "catch.hpp"
 
 #include <shadow.hpp>
+#include <utility>
 
 
+namespace tv_space
+{
 REGISTER_TYPE_BEGIN()
 REGISTER_TYPE_END()
 
 SHADOW_INIT()
+}
 
 
-TEST_CASE("test special member functions of shadow::variable", "[variable]")
+TEST_CASE("default construct a variable", "[variable]")
 {
     shadow::variable a;
 
     REQUIRE(a.has_value() == false);
+
+    SECTION("copy construct another variable from a")
+    {
+        shadow::variable b = a;
+
+        REQUIRE(b.has_value() == false);
+    }
+
+    SECTION("move construct another variable from a")
+    {
+        shadow::variable b = std::move(a);
+    }
+
+    SECTION("default constructo another variable")
+    {
+        shadow::variable b;
+
+        SECTION("copy assign a to b")
+        {
+            b = a;
+
+            REQUIRE(b.has_value() == false);
+        }
+
+        SECTION("move assign a to b")
+        {
+            b = std::move(a);
+
+            REQUIRE(b.has_value() == false);
+        }
+    }
 }
