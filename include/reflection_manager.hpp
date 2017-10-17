657939c9ea2bdca8be85a501453d324ee0c6ce37
Harald August Bergesen <bergesenha@gmail.com>
2017-10-17 12:07:52 +0200

    Add struct array_selector to specialize for array and pointer types. 	modified:   include/reflection_manager.hpp

--- include/reflection_manager.hpp
+++ include/reflection_manager.hpp
@@ -5,6 +5,34 @@
 
 namespace shadow
 {
+
+
+namespace reflection_initialization_detail
+{
+template <class ArrayType>
+struct array_selector;
+
+template <class T, std::size_t N>
+struct array_selector<T[N]>
+{
+    static helene::array_view<T> initialize(T (&arr)[N])
+    {
+        return helene::make_array_view(arr);
+    }
+};
+
+template <class T>
+struct array_selector<T*>
+{
+    static helene::array_view<T>
+    initialize(T* ptr)
+    {
+        return helene::array_view<T>();
+    }
+};
+}
+
+
 class reflection_manager
 {
 public:
@@ -15,13 +43,13 @@ public:
               class MemberFunctionArray,
               class MemberVariableArray,
               class StringSerializationArray>
-    reflection_manager(TypeInfoArray&,
-                       ConstructorInfoArray&,
-                       ConversionInfoArray&,
-                       FreeFunctionArray&,
-                       MemberFunctionArray&,
-                       MemberVariableArray&,
-                       StringSerializationArray&);
+    reflection_manager(TypeInfoArray& ti_arr,
+                       ConstructorInfoArray& ci_arr,
+                       ConversionInfoArray& cv_arr,
+                       FreeFunctionArray& ff_arr,
+                       MemberFunctionArray& mf_arr,
+                       MemberVariableArray& mv_arr,
+                       StringSerializationArray& ss_arr);
 
 private:
     helene::array_view<const type_info> type_info_view_;
