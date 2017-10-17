2f77994e3197998717b790d73db187d70fd7a3aa
Harald August Bergesen <bergesenha@gmail.com>
2017-10-17 12:53:45 +0200

    Add comparison operators to type_tag. 	modified:   include/api_types.hpp

--- include/api_types.hpp
+++ include/api_types.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <cstddef>
+#include <cstring>
 
 #include <any.hpp>
 #include <reflection_info.hpp>
@@ -15,6 +16,9 @@ class type_tag
 public:
     std::string name() const;
     std::size_t size() const;
+
+    bool operator==(const type_tag& other) const;
+    bool operator!=(const type_tag& other) const;
 
 private:
     const type_info* info_ptr_;
@@ -43,5 +47,26 @@ inline std::size_t
 type_tag::size() const
 {
     return info_ptr_->size;
+}
+
+inline bool
+type_tag::operator==(const type_tag& other) const
+{
+    if(info_ptr_ == other.info_ptr_)
+    {
+        return true;
+    }
+    else if(std::strcmp(info_ptr_->name, other.info_ptr_->name) == 0)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+inline bool
+type_tag::operator!=(const type_tag& other) const
+{
+    return !operator==(other);
 }
 }
