a76c4c8a44822b8a6e6cbfb049236cfde5bfe685
Harald August Bergesen <bergesenha@gmail.com>
2017-10-18 09:20:16 +0200

    Add indexed_info_iterator_. 	modified:   include/info_iterators.hpp

--- include/info_iterators.hpp
+++ include/info_iterators.hpp
@@ -143,6 +143,156 @@ public:
 private:
     InfoType* current_;
 };
+
+
+template <class InfoType, class ProxyType>
+class indexed_info_iterator_
+{
+public:
+    typedef ProxyType value_type;
+    typedef std::ptrdiff_t difference_type;
+    typedef ProxyType reference;
+    typedef ProxyType pointer;
+    typedef std::random_access_iterator_tag iterator_category;
+
+public:
+    indexed_info_iterator_()
+        : current_index_(0), index_buffer_(nullptr), data_buffer_(nullptr)
+    {
+    }
+
+    indexed_info_iterator_(std::size_t current_index,
+                           const std::size_t* index_buffer,
+                           InfoType* data_buffer)
+        : current_index_(current_index),
+          index_buffer_(index_buffer),
+          data_buffer_(data_buffer)
+    {
+    }
+
+public:
+    ProxyType operator*() const
+    {
+        return ProxyType(data_buffer_ + index_buffer_[current_index_]);
+    }
+
+    ProxyType operator->() const
+    {
+        return ProxyType(data_buffer_ + index_buffer_[current_index_]);
+    }
+
+    ProxyType operator[](difference_type n) const
+    {
+        return ProxyType(data_buffer_ + index_buffer_[current_index_ + n]);
+    }
+
+    indexed_info_iterator_& operator++()
+    {
+        ++current_index_;
+        return *this;
+    }
+
+    const indexed_info_iterator_ operator++(int)
+    {
+        auto temp = *this;
+        ++(*this);
+        return temp;
+    }
+
+    indexed_info_iterator_& operator--()
+    {
+        --current_index_;
+        return *this;
+    }
+
+    const indexed_info_iterator_ operator--(int)
+    {
+        auto temp = *this;
+        --(*this);
+        return temp;
+    }
+
+    indexed_info_iterator_&
+    operator+=(difference_type n)
+    {
+        current_index_ += n;
+        return *this;
+    }
+
+    indexed_info_iterator_&
+    operator-=(difference_type n)
+    {
+        current_index_ -= n;
+        return *this;
+    }
+
+    indexed_info_iterator_
+    operator+(difference_type n) const
+    {
+        return indexed_info_iterator_(
+            current_index_ + n, index_buffer_, data_buffer_);
+    }
+
+    indexed_info_iterator_
+    operator-(difference_type n) const
+    {
+        return indexed_info_iterator_(
+            current_index_ - n, index_buffer_, data_buffer_);
+    }
+
+    difference_type
+    operator-(const indexed_info_iterator_& other) const
+    {
+        return current_index_ - other.current_index_;
+    }
+
+    friend indexed_info_iterator_
+    operator+(difference_type lhs, const indexed_info_iterator_& rhs)
+    {
+        return rhs + lhs;
+    }
+
+    bool
+    operator==(const indexed_info_iterator_& other) const
+    {
+        return current_index_ == other.current_index_;
+    }
+
+    bool
+    operator!=(const indexed_info_iterator_& other) const
+    {
+        return current_index_ != other.current_index_;
+    }
+
+    bool
+    operator<(const indexed_info_iterator_& other) const
+    {
+        return current_index_ < other.current_index_;
+    }
+
+    bool
+    operator>(const indexed_info_iterator_& other) const
+    {
+        return current_index_ > other.current_index_;
+    }
+
+    bool
+    operator<=(const indexed_info_iterator_& other) const
+    {
+        return current_index_ <= other.current_index_;
+    }
+
+    bool
+    operator>=(const indexed_info_iterator_& other) const
+    {
+        return current_index_ >= other.current_index_;
+    }
+
+private:
+    std::size_t current_index_;
+    const std::size_t* index_buffer_;
+    InfoType* data_buffer_;
+};
 }
 
 
@@ -164,6 +314,32 @@ struct iterator_traits<shadow::info_iterator_<InfoType, ProxyType>>
         typename shadow::info_iterator_<InfoType, ProxyType>::pointer pointer;
     typedef
         typename shadow::info_iterator_<InfoType, ProxyType>::iterator_category
+            iterator_category;
+};
+
+
+template <class InfoType, class ProxyType>
+struct iterator_traits<shadow::indexed_info_iterator_<InfoType, ProxyType>>
+{
+    typedef
+        typename shadow::indexed_info_iterator_<InfoType, ProxyType>::value_type
+            value_type;
+
+    typedef typename shadow::indexed_info_iterator_<InfoType,
+                                                    ProxyType>::difference_type
+        difference_type;
+
+    typedef
+        typename shadow::indexed_info_iterator_<InfoType, ProxyType>::reference
+            reference;
+
+    typedef
+        typename shadow::indexed_info_iterator_<InfoType, ProxyType>::pointer
+            pointer;
+
+    typedef
+        typename shadow::indexed_info_iterator_<InfoType,
+                                                ProxyType>::iterator_category
             iterator_category;
 };
 }
