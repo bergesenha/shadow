88549e0cab39703363cf977a8ab9c53e224411df
Harald August Bergesen <bergesenha@gmail.com>
2017-10-19 15:29:37 +0200

    Add unit test for passing object between namespaces managers. 	modified:   tests/test_compile_time1.cpp

--- tests/test_compile_time1.cpp
+++ tests/test_compile_time1.cpp
@@ -6,6 +6,8 @@
 class tct1_class
 {
 public:
+    tct1_class() = default;
+
     tct1_class(int i) : i_(i)
     {
     }
@@ -34,6 +36,8 @@ REGISTER_TYPE_BEGIN()
 REGISTER_TYPE(tct1_class)
 REGISTER_TYPE_END()
 
+REGISTER_CONSTRUCTOR(tct1_class)
+
 REGISTER_FREE_FUNCTION(extract_i)
 
 SHADOW_INIT()
@@ -58,4 +62,35 @@ TEST_CASE("create an int using static_construct", "[static_construct]")
     auto anint = tct1_space::static_construct<int>(23);
 
     REQUIRE(anint.type().name() == std::string("int"));
+
+    SECTION(
+        "find constructor for tct1_class from tct1_space2 namespace manager")
+    {
+        auto constructors = tct1_space2::manager.constructors();
+
+        auto found = std::find_if(
+            constructors.first, constructors.second, [](const auto& ct) {
+                const auto name =
+                    tct1_space2::manager.constructor_type(ct).name();
+
+                return name == std::string("tct1_class");
+            });
+
+        auto paramtypes =
+            tct1_space2::manager.constructor_parameter_types(*found);
+
+        REQUIRE(found != constructors.second);
+        REQUIRE(std::distance(paramtypes.first, paramtypes.second) == 1);
+        REQUIRE(paramtypes.first->name() == std::string("int"));
+
+        SECTION("construct a tct1_class with int from tct1_space namespace")
+        {
+            std::vector<shadow::object> args{anint};
+
+            auto res = tct1_space2::manager.construct_object(
+                *found, args.begin(), args.end());
+
+            REQUIRE(res.type().name() == std::string("tct1_class"));
+        }
+    }
 }
