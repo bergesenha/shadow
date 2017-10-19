2227a6bcbd7a2dd09751f5e3409e12c7f02435c5
Harald August Bergesen <bergesenha@gmail.com>
2017-10-19 15:11:29 +0200

    More setup for unit testing. 	modified:   tests/test_compile_time1.cpp

--- tests/test_compile_time1.cpp
+++ tests/test_compile_time1.cpp
@@ -3,10 +3,52 @@
 #include <shadow.hpp>
 
 
+class tct1_class
+{
+public:
+    tct1_class(int i) : i_(i)
+    {
+    }
+
+    int
+    get_i() const
+    {
+        return i_;
+    }
+
+private:
+    int i_;
+};
+
+
+int
+extract_i(const tct1_class& a)
+{
+    return a.get_i();
+}
+
+
 namespace tct1_space
 {
 REGISTER_TYPE_BEGIN()
+REGISTER_TYPE(tct1_class)
 REGISTER_TYPE_END()
+
+REGISTER_FREE_FUNCTION(extract_i)
+
+SHADOW_INIT()
+}
+
+
+namespace tct1_space2
+{
+REGISTER_TYPE_BEGIN()
+REGISTER_TYPE(tct1_class)
+REGISTER_TYPE_END()
+
+REGISTER_CONSTRUCTOR(tct1_class, int)
+
+REGISTER_MEMBER_FUNCTION(tct1_class, get_i)
 
 SHADOW_INIT()
 }
