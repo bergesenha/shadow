35765a0f76aca1260f6b7b46520aa1b855e52013
Harald August Bergesen <bergesenha@gmail.com>
2017-10-20 14:15:37 +0200

    Add unit tests. 	modified:   tests/test_compile_time1.cpp

--- tests/test_compile_time1.cpp
+++ tests/test_compile_time1.cpp
@@ -322,5 +322,42 @@ TEST_CASE("create an int using static_construct", "[static_construct]")
                 }
             }
         }
+
+        SECTION("find tct1_class::set_i")
+        {
+            auto found =
+                std::find_if(mfs.first, mfs.second, [](const auto& mf) {
+                    return tct1_space2::manager.member_function_name(mf) ==
+                               std::string("set_i") &&
+                           tct1_space2::manager.member_function_class(mf)
+                                   .name() == std::string("tct1_class");
+                });
+
+            REQUIRE(found != mfs.second);
+
+            SECTION("get parameter types")
+            {
+                auto param_types =
+                    tct1_space2::manager.member_function_parameter_types(
+                        *found);
+
+                REQUIRE(std::distance(param_types.first, param_types.second) ==
+                        1);
+                REQUIRE(param_types.first->name() == std::string("int"));
+
+                SECTION("call set_i on a ctc1_class object from same namespace")
+                {
+                    auto obj = tct1_space2::static_construct<tct1_class>(33);
+
+                    auto res = tct1_space2::manager.call_member_function(
+                        obj, *found, &anint, &anint + 1);
+
+                    REQUIRE(res.type().name() == std::string("void"));
+                    REQUIRE(
+                        tct1_space2::get_held_value<tct1_class>(obj).get_i() ==
+                        23);
+                }
+            }
+        }
     }
 }
