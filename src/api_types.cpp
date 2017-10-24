769ca4c165befd6ad8586f805859dfef9bfedb45
Harald August Bergesen <bergesenha@gmail.com>
2017-10-24 14:22:02 +0200

    Implement rest of ostream << operator for object. 	modified:   src/api_types.cpp

--- src/api_types.cpp
+++ src/api_types.cpp
@@ -28,12 +28,14 @@ constexpr const type_info object::void_info;
 std::ostream&
 operator<<(std::ostream& out, const object& obj)
 {
+    // check for empty condition
     if(obj.manager_ == nullptr)
     {
         out << "void";
         return out;
     }
 
+    // find string serializer
     const auto index = obj.manager_->index_of_object(obj);
     const auto& serializers = obj.manager_->string_serialization_info_view_;
 
@@ -48,7 +50,32 @@ operator<<(std::ostream& out, const object& obj)
         return out;
     }
 
+    out << '{';
 
+    // if no string serializer, recurse through all member variables
+    auto mem_vars = obj.manager_->member_variables_by_class_type(obj.type());
+
+    if(std::distance(mem_vars.first, mem_vars.second) <= 1)
+    {
+        std::for_each(
+            mem_vars.first, mem_vars.second, [&obj, &out](const auto& mv) {
+                out << obj.manager_->get_member_variable(obj, mv);
+            });
+
+        out << '}';
+        return out;
+    }
+
+    out << obj.manager_->get_member_variable(obj, *mem_vars.first);
+    ++mem_vars.first;
+
+    std::for_each(
+        mem_vars.first, mem_vars.second, [&obj, &out](const auto& mv) {
+            out << ", ";
+            out << obj.manager_->get_member_variable(obj, mv);
+        });
+
+    out << '}';
     return out;
 }
 }
