d2167f4fb6b8c17c1491273126098263db9cafad
Harald August Bergesen <bergesenha@gmail.com>
2017-10-27 11:17:13 +0200

    Add struct for experimenting. 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
 #include <shadow.hpp>
+struct a
+{
+    int i;
+    double d;
+};
 
 
 namespace myspace
 {
 REGISTER_TYPE_BEGIN()
-
+REGISTER_TYPE(a)
 REGISTER_TYPE_END()
 
+
+REGISTER_MEMBER_VARIABLE(a, i)
+REGISTER_MEMBER_VARIABLE(a, d)
 
 SHADOW_INIT()
 } // namespace myspace
