47fa07f31e09efecd06838e0bb9e5cfc6c77c488
Harald August Bergesen <bergesenha@gmail.com>
2017-11-15 19:25:18 +0100

    experimenting. 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,10 @@ long_function(int i, const int& ii, int* iii)
 {
 }
 
+void void_fun()
+{
+}
+
 namespace myspace
 {
 REGISTER_TYPE_BEGIN()
@@ -36,6 +40,7 @@ REGISTER_TYPE_END()
 
 REGISTER_FREE_FUNCTION(get_i)
 REGISTER_FREE_FUNCTION(long_function)
+REGISTER_FREE_FUNCTION(void_fun)
 
 REGISTER_MEMBER_VARIABLE(a, i)
 REGISTER_MEMBER_VARIABLE(a, d)
@@ -72,16 +77,56 @@ p(const shadow::type_description& desc)
     std::cout << '\n';
 }
 
+namespace shadow
+{
+std::ostream&
+operator<<(std::ostream& out, const type_description& td)
+{
+    out << myspace::type_name_array_holder::value[td.type_index] << " ";
+    for(auto i = 0ul ; i < td.num_attributes; ++i)
+    {
+        auto att = td.attributes[i];
+
+        if(att == type_attribute::const_tag)
+        {
+            out << "const ";
+        }
+        if(att == type_attribute::pointer_tag)
+        {
+            out << "* ";
+        }
+        if(att == type_attribute::lreference_tag)
+        {
+            out << "& ";
+        }
+        if(att == type_attribute::rreference_tag)
+        {
+            out << "&& ";
+        }
+    }
+    return out;
+}
+
+std::ostream& operator<<(std::ostream& out, const free_function_info& ffi)
+{
+    out << *ffi.return_type << " ";
+    out << ffi.name << '(';
+    
+    for(auto i = 0ul; i < ffi.num_parameters ; ++i)
+    {
+        out << ffi.parameter_types[i] << ", ";
+    }
+
+    out << ')';
+
+    return out;
+}
+}
+
 int
 main()
 {
-    auto ffinfo = shadow::extract_free_function_info<
-        myspace::compile_time_ff_info<38>>::value;
-
-    std::cout << ffinfo.name << '\n';
-    p(*ffinfo.return_type);
-    for(auto i = 0ul ; i < ffinfo.num_parameters ; ++i)
-    {
-        p(ffinfo.parameter_types[i]);
-    }
+    std::for_each(std::begin(myspace::free_function_info_array_holder::value),
+                  std::end(myspace::free_function_info_array_holder::value),
+                  [](const auto& td) { std::cout << td << '\n'; });
 }
