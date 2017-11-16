e8cd3f30bb18d2dd29a586ee0f2d743246218c50
Harald August Bergesen <bergesenha@gmail.com>
2017-11-16 20:12:56 +0100

    Add get_specializer nested class template. 	modified:   include/any_reference.hpp

--- include/any_reference.hpp
+++ include/any_reference.hpp
@@ -8,14 +8,23 @@ class any_reference
 {
 public:
     template <class T>
-    any_reference(T& value) : value_ptr_(&value)
+    struct get_specializer
     {
-    }
+        typedef std::decay_t<T>& return_type;
+    };
 
     template <class T>
-    any_reference(const T& value) : value_ptr_(&value)
+    struct get_specializer<T&&>
     {
-    }
+        typedef std::decay_t<T>&& return_type;
+    };
+
+public:
+    template <class T>
+    any_reference(T& value);
+
+    template <class T>
+    any_reference(const T& value);
 
     any_reference(const any_reference&) = default;
     any_reference(any_reference&&) = default;
@@ -23,7 +32,25 @@ public:
     any_reference& operator=(any_reference&&) = default;
     ~any_reference() = default;
 
+public:
+    template <class T>
+    typename get_specializer<T>::return_type get();
+
 private:
     any value_ptr_;
 };
+}
+
+
+namespace shadow
+{
+template <class T>
+any_reference::any_reference(T& value) : value_ptr_(&value)
+{
+}
+
+template <class T>
+any_reference::any_reference(const T& value) : value_ptr_(&value)
+{
+}
 }
