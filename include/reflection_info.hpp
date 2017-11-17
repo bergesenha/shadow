2748cf558ae41521785a39585e1666b86c427b06
Harald August Bergesen <bergesenha@gmail.com>
2017-11-17 09:08:03 +0100

    Remove comparison operators for info structs. 	modified:   include/reflection_info.hpp

--- include/reflection_info.hpp
+++ include/reflection_info.hpp
@@ -21,11 +21,6 @@ struct type_info
     dereference_signature dereference_bind_point;
 };
 
-inline bool
-operator==(const type_info& lhs, const type_info& rhs)
-{
-    return std::string(lhs.name) == std::string(rhs.name);
-}
 
 typedef metamusil::t_descriptor::type_tag type_attribute;
 
@@ -46,12 +41,6 @@ struct constructor_info
     constructor_binding_signature bind_point;
 };
 
-inline bool
-operator==(const constructor_info& lhs, const constructor_info& rhs)
-{
-    return lhs.bind_point == rhs.bind_point;
-}
-
 
 struct conversion_info
 {
@@ -59,12 +48,6 @@ struct conversion_info
     std::size_t to_type_index;
     conversion_binding_signature bind_point;
 };
-
-inline bool
-operator==(const conversion_info& lhs, const conversion_info& rhs)
-{
-    return lhs.bind_point == rhs.bind_point;
-}
 
 
 struct free_function_info
@@ -75,12 +58,6 @@ struct free_function_info
     const type_description* parameter_types;
     free_function_binding_signature bind_point;
 };
-
-inline bool
-operator==(const free_function_info& lhs, const free_function_info& rhs)
-{
-    return lhs.bind_point == rhs.bind_point;
-}
 
 
 struct member_function_info
@@ -94,12 +71,6 @@ struct member_function_info
     member_function_binding_signature bind_point;
 };
 
-inline bool
-operator==(const member_function_info& lhs, const member_function_info& rhs)
-{
-    return lhs.bind_point == rhs.bind_point;
-}
-
 
 struct member_variable_info
 {
@@ -111,13 +82,6 @@ struct member_variable_info
     member_variable_set_binding_signature set_bind_point;
 };
 
-inline bool
-operator==(const member_variable_info& lhs, const member_variable_info& rhs)
-{
-    return lhs.get_bind_point == rhs.get_bind_point &&
-           lhs.set_bind_point == rhs.set_bind_point;
-}
-
 
 struct serialization_info
 {
@@ -126,8 +90,6 @@ struct serialization_info
     serialization_signature serialization_bind_point;
     deserialization_signature deserialization_bind_point;
 };
-
-
 } // namespace shadow
 
 #endif
