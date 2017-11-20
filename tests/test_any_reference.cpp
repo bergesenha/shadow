b61f7fb160e341f8c5f7853e42672c7541e74a64
Harald August Bergesen <bergesenha@gmail.com>
2017-11-20 16:03:58 +0100

    Add unit test. 	modified:   tests/test_any_reference.cpp

--- tests/test_any_reference.cpp
+++ tests/test_any_reference.cpp
@@ -1,2 +1,17 @@
 #include "catch.hpp"
 #include <any_reference.hpp>
+
+
+TEST_CASE("create any_reference from any", "[any_reference]")
+{
+    using shadow::any;
+    using shadow::any_reference;
+
+    any a = 10;
+
+    any_reference ra(a);
+
+    CHECK(ra.get<int>() == 10);
+    CHECK(&ra.get<int>() == &a.get<int>());
+}
+
