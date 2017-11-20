70bce115d60acda66754d9a4eaf14b40001d86ac
Harald August Bergesen <bergesenha@gmail.com>
2017-11-20 15:56:54 +0100

    Reimplement any_reference. 	modified:   include/any_reference.hpp

--- include/any_reference.hpp
+++ include/any_reference.hpp
@@ -6,6 +6,38 @@ namespace shadow
 {
 class any_reference
 {
+public:
+    any_reference() : ref_(nullptr)
+    {
+    }
+
+    any_reference(any& value) : ref_(&value)
+    {
+    }
+
+    any_reference& operator=(const any& value)
+    {
+        *ref_ = value;
+        return *this;
+    }
+
+public:
+    template <class T>
+    std::decay_t<T>&
+    get()
+    {
+        return ref_->get<T>();
+    }
+
+    template <class T>
+    const std::decay_t<T>&
+    get() const
+    {
+        return ref_->get<T>();
+    }
+
+private:
+    any* ref_;
 };
 }
 
